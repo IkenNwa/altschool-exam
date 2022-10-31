997866abf1c1b5a2e5109fc8b5650472b79286ce
Ikechukwu Nwachukwu <cfingers1chukwu@gmail.com>
2022-10-31 07:16:04 +0100

    Added 404 test

--- src/components/MiniNav.jsx
+++ src/components/MiniNav.jsx
@@ -6,6 +6,7 @@ function MiniNav() {
       <Link to="/dashboard/all">All</Link>
       <Link to="/dashboard/work">Work</Link>
       <Link to="/dashboard/casual">Casual</Link>
+      <Link to="/dashboard/others">Others</Link> 
     </>
   );
 }
