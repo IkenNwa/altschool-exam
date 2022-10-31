a14b3e4231bceb4fb1499f6c402cde110e532553
Ikechukwu Nwachukwu <cfingers1chukwu@gmail.com>
2022-10-31 06:00:23 +0100

    Removed Error Boundary Error

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,26 +1,15 @@
-import { useContext } from "react";
 import { Outlet } from "react-router-dom";
-import ErrorBoundary from "./ErrorBoundary";
 import MiniNav from "./MiniNav";
 import Navigation from "./Navigation";
-import { UserContext } from "./UserContext";
 
 function DashBoard() {
-    const { user, setUser } = useContext(UserContext);
-    if(!user) {
-        throw new ErrorBoundary
-    }else{
-        return
-    }
 
   return (
     <div>
       <Navigation />
-        <ErrorBoundary>
           <h1>Dashboard here</h1>
           <MiniNav />
           <Outlet />
-        </ErrorBoundary>
     </div>
   );
 }
