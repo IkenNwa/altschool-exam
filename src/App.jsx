61f1a13b64d7b13a45c30a2ec26ca0159511f74d
Ikechukwu Nwachukwu <cfingers1chukwu@gmail.com>
2022-11-03 03:36:56 +0100

    Installed React Helmet

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,22 @@ import "./App.css";
 import { UserContext } from "./components/UserContext";
 import { useState } from "react";
 import { useMemo } from "react";
+import { HelmetProvider } from "react-helmet-async";
 
 function App() {
   const [user, setUser] = useState(null);
 
-  const providerUser = useMemo(() => ({user, setUser}), [user, setUser])
+  const providerUser = useMemo(() => ({ user, setUser }), [user, setUser]);
+
+  const helmetContext = {}
 
   return (
     <>
-      <UserContext.Provider value={providerUser} >
-        <RouterApp />
-      </UserContext.Provider>
+      <HelmetProvider context={helmetContext}>
+        <UserContext.Provider value={providerUser}>
+          <RouterApp />
+        </UserContext.Provider>
+      </HelmetProvider>
     </>
   );
 }
